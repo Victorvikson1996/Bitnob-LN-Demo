7d84bf04e6340605927678cf7835be1dbf084b16
Victor Ezea <victorvikson@gmail.com>
2022-05-09 15:51:56 +0100

    converting class to componets

--- src/Navigations/TabNavigation.js
+++ src/Navigations/TabNavigation.js
@@ -1,6 +1,7 @@
 import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Entypo';
+import {TabIcon} from '../components';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ConfirmScreen, ReceiveScreen, SendScreen} from '../screens';
@@ -12,7 +13,7 @@ const screenOptions = {
   // tabBarActiveTintColor: '#6aeb76',
   tabBarShowLabel: false,
   tabBarStyle: {
-    backgroundColor: '#fff',
+    backgroundColor: '#102021',
     position: 'absolute',
     bottom: 25,
     right: 20,
@@ -21,37 +22,74 @@ const screenOptions = {
     borderRadius: 15, // for android
     height: 90,
   },
-  tabBarIcon({focused, name}) {
-    return (
-      <View>
-        <Image
-          source={require('../../assets/icons/send.png')}
-          resizeMode="contain"
-          style={{
-            width: 25,
-            height: 25,
-            tintColor: focused ? '#6aeb76' : '#fff',
-          }}
-        />
+  // tabBarIcon({focused, name}) {
+  //   return (
+  //     <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+  //       <Image
+  //         source={require('../../assets/icons/send.png')}
+  //         resizeMode="contain"
+  //         style={{
+  //           width: 25,
+  //           height: 25,
+  //           tintColor: focused ? '#6aeb76' : '#748c94',
+  //         }}
+  //       />
 
-        <Text
-          style={{
-            color: focused ? '#6aeb76' : '#fff',
-          }}
-        >
-          Send
-        </Text>
-      </View>
-    );
-  },
+  //       <Text
+  //         style={{
+  //           color: focused ? '#6aeb76' : '#748c94',
+  //           fontSize: 12,
+  //         }}
+  //       >
+  //         Send
+  //       </Text>
+  //     </View>
+  //   );
+  // },
 };
 export default function TabNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOptions} style={styles.shadow}>
-        <Tab.Screen name="Send" component={SendScreen} />
-        <Tab.Screen name="Receive" component={ReceiveScreen} />
-        <Tab.Screen name="Confirm" component={ConfirmScreen} />
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen
+          name="Send"
+          component={SendScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <TabIcon
+                focused={focused}
+                icon={require('../../assets/icons/bitcoin-wallet.png')}
+                label="wallet"
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Receive"
+          component={ReceiveScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <TabIcon
+                focused={focused}
+                icon={require('../../assets/icons/send.png')}
+                label="send"
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Confirm"
+          component={ConfirmScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <TabIcon
+                focused={focused}
+                icon={require('../../assets/icons/scan.png')}
+                label="scan"
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/screens/SendScreen.js
+++ src/screens/SendScreen.js
@@ -1,11 +1,43 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native';
+import React, {useState, useEffect, useCallback} from 'react';
+import Bitnob from 'bitnob-react-native';
 
 const SendScreen = () => {
+  const [balance, setBalance] = useState(0);
+  const [isShowBitnob, setIsShowBitnob] = useState(false);
+  const [isShowQrCode, setIsShowQrCode] = useState(false);
+
+  const createCheckOut = () => {
+    return (
+      <Bitnob
+        baseUrl={'https://bitnob.com/api/v1/'}
+        description="Test108"
+        amount={108}
+        callbackUrl="https://bitnob.com/api/v1/callback"
+        notificationEmail="victorvikson@gmail.com"
+        customerEmail="victorvikson@gmail.com"
+        satoshis={1000}
+        reference="Test108"
+        publicKey="test"
+        failCallback={() => {
+          console.log('failCallback');
+          setIsShowBitnob(false);
+        }}
+        webViewCallback={res => {
+          console.log('webViewCallback', res);
+          isShowBitnob(false);
+        }}
+        successCallback={success => {
+          console.log('successCallback', success);
+        }}
+      />
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>SendScreen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
+      <StatusBar backgroundColor={'transparent'} translucent={false} />
+    </SafeAreaView>
   );
 };
 
@@ -14,7 +46,7 @@ export default SendScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#082d3b',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
